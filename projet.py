122f677cf70ff5e96845a955d476c6b9fb5990b3
MdRiabi <mdriabi@gmail.com>
2025-07-07 20:29:14 +0100

    Squashed commit of the following:
    commit 63c5fd0b4e271ded53a7d9405d7edf536d3cbd17
    Merge: 74897d6 2517044
    Author: MdRiabi <mdriabi@gmail.com>
    Date:   Mon Jul 7 20:17:41 2025 +0100
    
        Merge branch 'projetc2'
    
    commit 74897d6e83dfe902e9d6084a4af0ef5c176e4f1c
    Author: MdRiabi <mdriabi@gmail.com>
    Date:   Mon Jul 7 20:12:42 2025 +0100
    
        projet.py from main branch

--- projet.py
+++ projet.py
@@ -1,5 +1,9 @@
 
+def moyenne(T) s=0 for t in T: s++=t return S
+
+
 Data=[1,2,3]
+
 if Data:
  Moy=statistics.mean(Data)
  print('la moyenne est:', Moy)
@@ -8,3 +12,8 @@ if Data:
  print('la moyenne est:', Moy)
 else:
   print('dossier vide')
+
+Moy=statistics.mean(Data)
+print('la moyenne est:', Moy)
+ projetc2
+main
